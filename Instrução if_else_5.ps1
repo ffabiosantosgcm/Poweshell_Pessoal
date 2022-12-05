9b0a3c147850d81d99117f5257c780db3dbb9ff5
Fábio Santos Gomes <ffabiosantosgcm@gmail.com>
2022-12-05 13:18:20 -0300

    Instrução if_else_5.ps1

--- Instrução if_else_5.ps1
+++ Instrução if_else_5.ps1
@@ -1,4 +1,4 @@
-# Quarto problema! #
+# Problema 5 #
 
 <# Faça um algotimo para calcular o novo salário de um funcionário. Sabe-se que os funcionários que recebem
 atualmente salário de até R$ 1.045,00 terão aumento de 20%; os demais terão aumento de 10%.#>
