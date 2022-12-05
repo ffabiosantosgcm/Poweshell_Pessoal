f4116474f61ffce5a9ed0136234c4b8399d7464c
Fábio Santos Gomes <ffabiosantosgcm@gmail.com>
2022-12-05 11:37:51 -0300

    Instrução if_else

---
+++ Instrução if_else.ps1
@@ -0,0 +1,23 @@
+# Desenvolva um algoritmo para ler dois valores (considere que não serão lidos valores iguais)
+# e escrever o maior deles.
+
+[int]$cd = Read-Host "Digite um número"
+[int]$de = Read-Host "Digite outro número para comparar"
+
+if ($cd -eq $de) # primeiro if
+
+{
+
+Write-Output "Números são iguas!  Por favor, digite números diferentes para prosseguir."
+
+}
+
+else {
+  
+    if ($cd -gt $de) {Write-Host "$cd é maior"} # Segundo if
+    
+    else {
+        Write-Host "$de é maior"
+    }
+    
+}
